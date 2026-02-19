9bf90cb9ad56fc1e6a95afb391c4ae1f96fa40d3
jasonniebauer <lookwhatjasondid@gmail.com>
2026-02-18 18:44:01 -0600

    Cache model for performance

--- prediction.py
+++ prediction.py
@@ -1,6 +1,17 @@
+# User Interface
+import streamlit as st
+
+# Machine Learning Model Persistence
 import joblib
 
 
+@st.cache_resource(show_spinner="Loading classifier model...")
+def load_model():
+    model = joblib.load('random_forest_model.sav')
+    return model
+
+# Load once, reuse everywhere
+MODEL = load_model()
+
 def predict(data):
-    model_classifier = joblib.load('random_forest_model.sav')
-    return model_classifier.predict(data)
+    return MODEL.predict(data)
